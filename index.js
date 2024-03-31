616b1444d382d9b7caed3d01e66749dd151001d0
Klapeks <sashaKaraguts@gmail.com>
2024-03-31 10:35:10 +0300

    fix

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var swagger_1 = __importDefault(require("./swagger"));
+var swagger_1 = __importDefault(require("./dist/swagger"));
 exports.default = swagger_1.default;
